ab46b5b93b29125f0b4137453f0b76e27f0308ce
Abena-Dumfeh <47983899+Abena-Dumfeh@users.noreply.github.com>
2019-03-18 21:44:51 +0000

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.maame.assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
